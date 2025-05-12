59ee074e1a97764f2f3aefe1f8b20bcbf57afdb7
Elcossonator <cosson.antonin@gmail.com>
2025-05-12 20:24:10 +0200

    Fix multer upload middleware

--- Back/routes/form.js
+++ Back/routes/form.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const cloudinary = require('cloudinary').v2;
 
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
+
+const cloudinary = require('cloudinary').v2;
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
 
 // Import dynamique de fetch (compatible avec Node sur Render)
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
