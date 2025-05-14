d71893161af03023031ee2ef5b55963d2bf6d0c6
Elcossonator <cosson.antonin@gmail.com>
2025-05-14 21:17:27 +0200

    intro cropeer.html

--- Back/server.js
+++ Back/server.js
@@ -6,8 +6,9 @@ const formRoutes = require('./routes/form');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/api/form', formRoutes);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`🪑 API MissChair ready on port ${PORT}`));
+app.listen(PORT, () => console.log(`🪑 API MissChair ready on port ${PORT}`));t
